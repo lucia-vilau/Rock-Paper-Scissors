4448c7c76f8a46fa843024b46243a2e26a9dfca0
lucia-vilau <113890216+lucia-vilau@users.noreply.github.com>
2022-12-28 13:14:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,144 +1,66 @@
-// ------ PARTIE 1 -------
-/*'use strict';
-
-// Vous devez déclarer 3 variables,
-let player;
-
-let computer;
-
-let random;
-
-
-// demandez à l'utilisateur son choix (pierre, feuille ou ciseaux)
-player = window.prompt("Choisissez", "Pierre, feuille ou ciseaux?");
-// modifier la casse en minusule
-player = player.toLowerCase()
-*/
-
-
-// ------ PARTIE 2 ------
-/*'use strict';
-
-// Vous devez déclarer 3 variables,
-var player;
-var random;
-var computer;
-// demandez à l'utilisateur son choix (pierre, feuille ou ciseaux)
-player = window.prompt('Choisissez pierre feuille ou ciseaux');
-// modifier la casse en minusule
-player = player.toLowerCase();
-
-// affectez une valeur aleatoire entre 0 et 1 à la variable random
-// @see https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Math/random
-random = Math.random();
-// definissez le choix de l'ordianteur (1/3 de chance pour chacun des choix)
-let choice1 = 1/3;
-let choice2 = 2/3;
-let choice3 = 3/3;
-// si 1/3 affectez pierre à la variable computer
-if (random < 1/3) {
-	computer = 'pierre';
-}
-// sinon si 2/3 affectez feuille à la variable computer
-else if (random < 2/3) {
-	computer = 'feuille';
-}
-// sinon affectez ciseaux à la variable computer
-else {
-	computer = 'ciseaux';
-}
-
-// affichez le choix de l'ordinateur dans le document
-document.write(computer);
-*/
-
-
-//------ PARTIE 3 ------
-
 'use strict';
-// Vous devez déclarer 3 variables,
+
 var player;
 var random;
 var computer;
 
-// demandez à l'utilisateur son choix (pierre, feuille ou ciseaux)
-player = window.prompt('Choisissez pierre feuille ou ciseaux');
-// modifier la casse en minusule
+player = window.prompt('Choose', 'Rock, paper or scissors?');
+
 player = player.toLowerCase();
 
-// affectez une valeur aleatoire entre 0 et 1 à la variable random
-// @see https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Math/random
 random = Math.random();
 
-// definissez le choix de l'ordianteur (1/3 de chance pour chacun des choix)
-// si 1/3 affectez pierre à la variable computer
-if(random < 1 / 3)
-{
-    computer = 'pierre';
-}
-// sinon si 2/3 affectez feuille à la variable computer
-else if(random < 2 / 3) 
-{
-    computer = 'feuille';
-}
-// sinon affectez ciseaux à la variable computer
-else
-{
-    computer = 'ciseaux';
+if(random < 1 / 3) {
+    computer = 'Rock';
 }
 
-// affichez le choix de l'ordinateur dans le document
-document.write("<p>Choix de l'ordinateur : <strong>" + computer + "</strong></p>");
-
-
-// Vérifiez si il y a une égalité
-if (player == computer) {
-    // affichez l'égalité
-    document.write('égalité');
+else if(random < 2 / 3) {
+    computer = 'Paper';
 }
- 
-// sinon ...
+
 else {
+    computer = 'Scissors';
+}
 
-    //  ... vérifiez les différents cas possibles avec 
-    // un switch sur la variable computer
+document.write("<p>Computer's choice : <strong>" + computer + "</strong></p>");
+
+if (player == computer) {
+    document.write('That\'s a tie!');
+}
+
+else {
     switch(computer) {
-        // cas du choix pierre pour l'ordinateur
-        case "pierre" :
-            //si le joueur a choisi feuille
-        	if (player = "feuille") {
-                document.write("gagné")
-           }
-        
 
-          // sinon c'est qu'il a choisi ciseaux
+        case "Rock" :
+
+        	if (player = "Paper") {
+                document.write("Congratulations! You win!")
+            }
+
             else {
-                document.write("perdu")
+                document.write("Sorry, you lost!")
             }       
         break;
 
-        // cas choix feuille
-        case "feuille" :
-           // si le joueur a choisi pierre
-            if (player = "ciseaux") {
-                document.write("gagné")
+        case "Paper" :
+           
+            if (player = "Scissors") {
+                document.write("Congratulations! You win!")
             }
-            // sinon c'est qu'il a choisi ciseaux
+            
             else {
-                document.write("perdu")
+                document.write("Sorry, you lost!")
             }
         break;
         
-        
-        // cas du choix ciseaux
-        case "ciseaux" :
-            // si le joueur a choisi pierre
-            if (player = "pierre") {
-			    document.write("gagné")
+        case "Scissors" :
+            
+            if (player = "Rock") {
+			    document.write("Congratulations! You win!")
 		    }
-           // sinon c'est qu'il a choisi feuille
+           
             else {
-			    document.write("perdu")
+			    document.write("Sorry, you lost!")
 		    } 
         break;
     }
